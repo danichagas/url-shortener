f05815ab8247d1591be5e7177bbdbf0e1c860866
danichagas <chagasdaniel788@gmail.com>
2026-06-28 18:17:54 -0300

    feat: add global validation pipe and secure DTO with class-validator

--- src/url/dto/create-url.dto.ts
+++ src/url/dto/create-url.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsUrl } from "class-validator";
+
 export class CreateUrlDto {
+    @IsNotEmpty({ message: 'The original URL cannot be empty' })
+    @IsUrl({}, { message: 'The provided string must be a valid URL' })
     originalUrl!: string
 }
